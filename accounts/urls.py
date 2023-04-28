1741d734a8387861abe14ba2aeff04a498a3a211
hyundol2 <nhs503601@gmail.com>
2023-04-28 11:51:20 +0900

    Update accounts/view and Add migrations -NHS

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('update/', views.update, name='update'),
     path('password/', views.change_password, name='change_password'),
     path('profile/<str:username>', views.profile, name='profile'),
-    # path('<int:user_pk>/follow', views.follow, name='follow'),
+    path('<int:user_pk>/follow', views.follow, name='follow'),
 ]
