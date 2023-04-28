d7b868b0c199c58c0145610d2f8f39605bf0cc3c
SungBum Ha <gktjd425@naver.com>
2023-04-28 17:58:15 +0900

    tag_develop

--- dinings/models.py
+++ dinings/models.py
@@ -1,10 +1,36 @@
 from django.db import models
 from django.conf import settings
 from taggit.managers import TaggableManager
+from taggit.models import TaggedItemBase
 
 # Create your models here.
 def dining_img_path(instance, filename):
-    return f'images/{instance.title}/{filename}'
+    return f'images/dining/{instance.title}/{filename}'
+
+
+def review_img_path(instance, filename):
+    return f'images/review/{instance.dining}/{instance.user.username}/{filename}'
+
+
+class MenuTaggedItem(TaggedItemBase):
+    content_object = models.ForeignKey('Dining', on_delete=models.CASCADE, related_name='menu_tagged_items')
+
+
+class PriceTaggedItem(TaggedItemBase):
+    content_object = models.ForeignKey('Dining', on_delete=models.CASCADE, related_name='price_tagged_items')
+
+
+class PurposeTaggedItem(TaggedItemBase):
+    content_object = models.ForeignKey('Review', on_delete=models.CASCADE, related_name='purpose_tagged_items')
+
+
+class AtmosphereTaggedItem(TaggedItemBase):
+    content_object = models.ForeignKey('Review', on_delete=models.CASCADE, related_name='atmosphere_tagged_items')
+
+
+class FacilityaggedItem(TaggedItemBase):
+    content_object = models.ForeignKey('Review', on_delete=models.CASCADE, related_name='facility_tagged_items')
+
 
 class Dining(models.Model):
     title = models.CharField(max_length=50)
@@ -26,6 +52,8 @@ class Dining(models.Model):
     phone_number = models.CharField(max_length=20, blank=True)
     
     tags = TaggableManager(blank=True)
+    menu_tags = TaggableManager(blank=True, through=MenuTaggedItem, related_name='menu_tags')
+    price_tags = TaggableManager(blank=True, through=PriceTaggedItem, related_name='price_tags')
 
 
 class Review(models.Model):
@@ -35,13 +63,16 @@ class Review(models.Model):
     content = models.TextField(null=True)
 
     # 이미지는 5개까지 업로드 가능하게
-    image1 = models.ImageField(blank=True)
-    image2 = models.ImageField(blank=True)
-    image3 = models.ImageField(blank=True)
-    image4 = models.ImageField(blank=True)
-    image5 = models.ImageField(blank=True)
+    image1 = models.ImageField(blank=True, upload_to=review_img_path)
+    image2 = models.ImageField(blank=True, upload_to=review_img_path)
+    image3 = models.ImageField(blank=True, upload_to=review_img_path)
+    image4 = models.ImageField(blank=True, upload_to=review_img_path)
+    image5 = models.ImageField(blank=True, upload_to=review_img_path)
 
     rating = models.FloatField()
+    rating_taste = models.FloatField()
+    rating_price = models.FloatField()
+    rating_kind = models.FloatField()
 
     def half_star_rating(self):
         # 반올림한 별점 값을 구함
@@ -51,3 +82,6 @@ class Review(models.Model):
         return '★' * int(rounded_rating) + '☆' * (rounded_rating % 1 == 0.5)
     
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_reviews")
+    purpose_tags = TaggableManager(blank=True, through=PurposeTaggedItem, related_name='purpose_tags')
+    atmosphere_tags = TaggableManager(blank=True, through=AtmosphereTaggedItem, related_name='atmosphere_tags')
+    facility_tags = TaggableManager(blank=True, through=FacilityaggedItem, related_name='facility_tags')
