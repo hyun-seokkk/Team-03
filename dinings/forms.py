4e0c29e43eba219f7364bc7d2a4f5685dde49af9
SungBum Ha <gktjd425@naver.com>
2023-04-30 23:30:12 +0900

    star_rating

--- dinings/forms.py
+++ dinings/forms.py
@@ -9,7 +9,12 @@ class DiningForm(forms.ModelForm):
         exclude = ['like_users']
 
 class ReviewForm(forms.ModelForm):
+    rating = forms.FloatField(widget=forms.NumberInput(attrs={'step': 0.5, 'min':0, 'max':5}))
+    rating_taste = forms.FloatField(widget=forms.NumberInput(attrs={'step': 0.5, 'min':0, 'max':5}))
+    rating_price = forms.FloatField(widget=forms.NumberInput(attrs={'step': 0.5, 'min':0, 'max':5}))
+    rating_kind = forms.FloatField(widget=forms.NumberInput(attrs={'step': 0.5, 'min':0, 'max':5}))
+
     class Meta:
         model = Review
         fields = '__all__'
-        exclude = ['user', 'dinining', 'like_users',]
+        exclude = ['user', 'dining', 'like_users',]
